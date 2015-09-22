d44dd9ed027213ab53863e9497257b813664c50e
zemer <zemer@bidspirit.com>
2015-09-22 20:59:17 +0300

    z

--- portal/js/all.js
+++ portal/js/all.js
@@ -9197,6 +9197,7 @@ define("common/js/modules/domUtils/domUtilsModule", [ "angular" ], function(ng) 
             mViewPortElement = document.querySelector('meta[name="viewport"]'), $rootScope.viewPortDebugInfo = mDebugInfo, 
             window.addEventListener("resize", onResize), updateViewportInfo(), $timeout(updateViewportInfo, 1e3), 
             onResize();            
+            alert("splash? "+navigator.splashscreen);
             if (navigator.splashscreen){
             	alert("hideing splash.. "+navigator.splashscreen);            	
             	setTimeout(navigator.splashscreen.hide,500);
